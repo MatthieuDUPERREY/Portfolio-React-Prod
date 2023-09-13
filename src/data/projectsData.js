6ef2edfa2e23b754363e6d3301282a1d5ff4557b
Matthieu Duperrey <matth.duperrey@gmail.com>
2023-09-13 15:18:59 -0400

    --git a/.DS_Store b/.DS_Store

---
+++ src/data/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "Flappy Bird",
+    date: "Octobre 2022",
+    languages: ["HTML", "CSS3", "JavaScript"],
+    infos:
+      "Projet en JavaScript Vanilla, HTML et CSS. Le but était de recréer le jeu mobile Flappy Bird en JavaScript.",
+    img: "./assets/img/flappy-bird-set.png",
+    link: "http://www.google.com",
+  },
+  {
+    id: 2,
+    title: "Yoga Routine",
+    date: "Novembre 2022",
+    languages: ["HTML/CSS3", "JavaScript"],
+    infos:
+      "Yoga Routine est une application en Javascript Vanilla permettant de créér son propre programme d'étirements.",
+    img: "./assets/img/yoga-routine-app.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 3,
+    title: "Cooking App",
+    date: "Janvier 2023",
+    languages: ["HTML/CSS3", "Javascript", "React"],
+    infos:
+      "Cooking App est une application en React permettant de rechercher des recettes de cuisine. L'application utilise l'API de TheMealDB pour récupérer les recettes.",
+    img: "./assets/img/cooking-app-pic.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 4,
+    title: "Crypto Watcher",
+    date: "Avril 2023",
+    languages: ["React", "Redux"],
+    infos:
+      "Crypto Watcher est une application d'analyse du cours des cryptomonnaies. Son dashboard permet de suivre les cours des diffèrentes cryptomonnaie en temps réel, de connaître leur prix, leur volume d'échange, leur capitalisation boursière et leur évolution sur 24h.",
+    img: "./assets/img/crypto-app-pic.jpg",
+    link: "http://www.google.com",
+  },
+];
