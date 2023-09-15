8c56c32ea2657a4f247494d0f2f316aefcaabe76
Matthieu Duperrey <matth.duperrey@gmail.com>
2023-09-15 11:22:06 -0400

    Ajout des liens hypertext debug

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -7,7 +7,7 @@ export const projectsData = [
     infos:
       "Projet en JavaScript Vanilla, HTML et CSS. Le but était de recréer le jeu mobile Flappy Bird en JavaScript.",
     img: "./assets/img/flappy-bird-set.png",
-    link: "http://www.google.com",
+    link: "https://matthieuduperrey.github.io/Projet-Flappy-Bird-JS/",
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ export const projectsData = [
     infos:
       "Yoga Routine est une application en Javascript Vanilla permettant de créér son propre programme d'étirements.",
     img: "./assets/img/yoga-routine-app.jpg",
-    link: "http://www.google.com",
+    link: " https://matthieuduperrey.github.io/Yoga-Routine-App-JS/",
   },
   {
     id: 3,
@@ -27,7 +27,7 @@ export const projectsData = [
     infos:
       "Cooking App est une application en React permettant de rechercher des recettes de cuisine. L'application utilise l'API de TheMealDB pour récupérer les recettes.",
     img: "./assets/img/cooking-app-pic.jpg",
-    link: "http://www.google.com",
+    link: "https://cooking-app-react-matthieu-1e285a6ee036.herokuapp.com/",
   },
   {
     id: 4,
@@ -37,6 +37,6 @@ export const projectsData = [
     infos:
       "Crypto Watcher est une application d'analyse du cours des cryptomonnaies. Son dashboard permet de suivre les cours des diffèrentes cryptomonnaie en temps réel, de connaître leur prix, leur volume d'échange, leur capitalisation boursière et leur évolution sur 24h.",
     img: "./assets/img/crypto-app-pic.jpg",
-    link: "http://www.google.com",
+    link: "https://crypto-watcher-30cd39e5f88f.herokuapp.com/",
   },
 ];
